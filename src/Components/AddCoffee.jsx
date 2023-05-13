f21a59b4bf2f69784cdb5e41528fe18c7b753ada
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-05-13 22:01:27 +0600

    first commit

---
+++ src/Components/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h2>add coffee</h2>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App';
+import AddCoffee from './Components/AddCoffee';
+import UpdateCoffee from './Components/UpdateCoffee';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App></App>
+  },
+  {
+    path:'addCoffee',
+    element:<AddCoffee></AddCoffee>
+  },
+  {
+    path:'updateCoffee',
+    element:<UpdateCoffee></UpdateCoffee>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
